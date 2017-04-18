71c847bdff8cad1a02a8359768e56000b8e30e7e
cmnvb <superstar.kim@hotmail.com>
2017-04-18 12:37:50 +1000

    Index "find me" Functionality Added
    Added JS to index page.

---
+++ js/index.js
@@ -0,0 +1,43 @@
+function initialise() {
+	findMeButton = document.getElementById("findme");
+	findMeButton.addEventListener("click", findPosition);
+}
+
+function findPosition() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(postPosition, alertError);
+	} else {
+		alert("Geolocation is not supported by this browser.");
+	}
+}
+
+function postPosition(position) {
+	//TODO: Post to results page for searching
+
+	/*Placeholder*/ alert("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
+	window.location.href = "results.html";
+}
+
+function alertError(error) {
+	var msg = "";
+
+	switch(error.code) {
+		case error.PERMISSION_DENIED:
+			msg = "User denied the request for Geolocation."
+			break;
+		case error.POSITION_UNAVAILABLE:
+			msg = "Location information is unavailable."
+			break;
+		case error.TIMEOUT:
+			msg = "The request to get user location timed out."
+			break;
+		case error.UNKNOWN_ERROR:
+			msg = "An unknown error occurred."
+			break;
+	}
+
+	alert(msg);
+}
+
+// Initialise event listeners after page loading
+window.addEventListener("load", initialise);
