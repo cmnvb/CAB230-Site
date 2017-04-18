0580a0236547bb475b2d5e56eef5d23ab7feec70
cmnvb <superstar.kim@hotmail.com>
2017-04-18 16:49:14 +1000

    Register Form Validation and Colour
    Form validation added to account sign up. Colour changing from grey to
    black added for DOB and suburb.

--- js/greyTextFix.js
+++ js/greyTextFix.js
@@ -1,15 +1,13 @@
 function initialise() {
 	var suburb = document.getElementById("suburb");
+	var DOB = document.getElementById("DOB");
 
-	checkValid(suburb);
+	suburb.addEventListener("change", function(){suburb.style.color = "black"});
 
-	suburb.addEventListener("change", function(){checkValid(suburb)});
-}
-
-function checkValid(suburb) {
-	if (suburb.value != "") {
-		suburb.style.color = "black";
+	if (DOB) {
+		DOB.addEventListener("change", function(){suburb.style.color = "black"});
 	}
+
 }
 
 window.addEventListener("load", initialise);
