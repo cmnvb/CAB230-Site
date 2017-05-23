ef697d583a26cb9276dcf63ea68e45058862ca70
annaclatworthy <annajclatworthy@gmail.com>
2017-05-23 18:03:20 +1000

    just to make sure theres no conflict errors

--- php/signup.php
+++ php/signup.php
@@ -1,58 +1,60 @@
-<!DOCTYPE html>
+<?php require('connectToDB.php'); ?><!DOCTYPE html>
 <html>
-<head>
-	<!-- Page Data -->
-	<meta charset="utf-8" />
-	<title>Account Signup</title>
-
-	<!-- Styling -->
-	<link rel="stylesheet" type="text/css" href="../css/style.css">
-	<link rel="stylesheet" type="text/css" href="../css/signup.css">
-	<link href="https://fonts.googleapis.com/css?family=Kaushan+Script%7COpen+Sans" rel="stylesheet">
-
-	<!-- Scripts -->
-	<script type="text/javascript" src="../js/greyTextFix.js"></script>
-</head>
-
-<body>
-	<!-- Nav-->
-	<?php include('navbar.php'); ?>
-
-	<main>
-		<div id="wrapper">
-			<div id="crumb">
-				<p><a href="index.php">Search</a> > Make An Account</p>
-				<div id="header">
-					<h1 id="header-text">Create An Account</h1>
+	<head>
+		<!-- Page Data -->
+		<meta charset="utf-8" />
+		<title>Account Signup</title>
+	
+		<!-- Styling -->
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
+		<link rel="stylesheet" type="text/css" href="../css/signup.css">
+		<link href="https://fonts.googleapis.com/css?family=Kaushan+Script%7COpen+Sans" rel="stylesheet">
+	
+		<!-- Scripts -->
+		<script type="text/javascript" src="../js/greyTextFix.js"></script>
+	</head>
+	
+	<body>
+		<!-- Nav-->
+		<?php include('navbar.php'); ?>
+	
+		<main>
+			<div id="wrapper">
+				<div id="crumb">
+					<p><a href="index.php">Search</a> > Make An Account</p>
+					<div id="header">
+						<h1 id="header-text">Create An Account</h1>
+					</div>
+					<form id="registration-form" action="index.php" method="post">
+	
+						<input type="text" name="username" placeholder="Username" required/><hr>
+					
+						<input type="email" name="email" placeholder="Email Address" required/><hr>
+						
+						<input type="number" name="children" placeholder="Number of Children" min="0" required/><hr>
+						
+						<input id="DOB" type="date" name="bday" placeholder="Date of Birth dd/mm/yy" onchange="this.style.color = 'black'" required/><hr>
+						
+						<select id="suburb" name="suburb" required>
+							<option value="" selected hidden>Select A Suburb</option>
+							<option value="place">place</option>
+							<option value="holder">holder</option>
+						</select><hr>
+						
+						<input type="text" name="password" placeholder="Insert Password" title="Password must be contain at least 4 characters and include a capital letter" pattern="(?=.*[a-z])(?=.*[A-Z]).{4,}" onchange="form.passwordConfirm.pattern = this.value;" required/><hr>
+						
+						<input type="text" name="passwordConfirm" placeholder="Confirm Password" title="Must match entered password" pattern="(?=.*[a-z])(?=.*[A-Z]).{4,}" required/><hr>
+						<br>
+						
+						<input type="submit" value="Send" />
+						<?php include('validatesignupform.php'); ?>
+					</form>
 				</div>
-				<form id="registration-form" action="index.php" method="post">
-					<input type="text" name="username" placeholder="Username" required/><hr>
-				
-					<input type="email" name="email" placeholder="Email Address" required/><hr>
-					
-					<input type="number" name="children" placeholder="Number of Children" min="0" required/><hr>
-					
-					<input id="DOB" type="date" name="bday" placeholder="Date of Birth dd/mm/yy" onchange="this.style.color = 'black'" required/><hr>
-					
-					<select id="suburb" name="suburb" required>
-						<option value="" selected hidden>Select A Suburb</option>
-						<option value="place">place</option>
-						<option value="holder">holder</option>
-					</select><hr>
-					
-					<input type="text" name="password" placeholder="Insert Password" title="Password must be contain at least 4 characters and include a capital letter" pattern="(?=.*[a-z])(?=.*[A-Z]).{4,}" onchange="form.passwordConfirm.pattern = this.value;" required/><hr>
-					
-					<input type="text" name="passwordConfirm" placeholder="Confirm Password" title="Must match entered password" pattern="(?=.*[a-z])(?=.*[A-Z]).{4,}" required/><hr>
-					<br>
-					
-					<input type="submit" value="Send" />
-				</form>
 			</div>
-		</div>
-	</main>
-
-	<!-- Footer -->
-	<footer>
-	</footer>
-</body>
+		</main>
+	
+		<!-- Footer -->
+		<footer>
+		</footer>
+	</body>
 </html>
--- php/validatesignupform.php
+++ php/validatesignupform.php
@@ -1,18 +1,6 @@
-<?php
+<?php require('connectToDB.php'); 
 $username = $email = $children= $bday = $suburb = $password = "";
-//$usernameErr = $emailErr = $childrenErr = $bdayErr = $suburbErr = $passwordErr = $passwordConfirmErr ="";
-//  if (empty($_POST["username"])) {
-//    $usernameErr = "Username is required";
-//  } else {
-//    $username = validate($_POST["username"]);
-//  }
-//  
-//  if(empty($_POST['children'])){
-//    $childrenErr = "Number of Children is required";
-//    } else {
-//    $children = validate($_POST['children'])
-//  }
-//}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $username = validate($_POST["username"]);
   $email = validate($_POST["email"]);
@@ -20,6 +8,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $bday = validate($_POST["bday"]);
   $suburb = validate($_POST["suburb"]);
   $password = validate($_POST["password"]);
+  sendInfo();
+
 
 function validate($data) {
     $data = trim($data);
@@ -28,6 +18,15 @@ function validate($data) {
     return $data;
 }
 
-//$sql = "INSERT INTO members (Username, Email, NoOfChildren, Birthday, Suburb, Password, Salt)
-//        VALUES ('$username', '$email', '$children', '$bday', '$suburb', '$password')";
+function sendInfo(){
+$stmt =  $pdo ->prepare ('INSERT INTO members (Username, Email, NoOfChildren, Birthday, Suburb, Password, Salt)
+       VALUES (:username, :email, :children, :bday, :suburb, :password)');
+$stmt ->bindParam(':username',$username);
+$stmt ->bindParam(':email',$email);
+$stmt ->bindParam(':children',$children);
+$stmt ->bindParam(':bday',$bday);
+$stmt ->bindParam(':suburb',$suburb);
+$stmt ->bindParam(':password',$password);
+$stmt -> execute();
+}
 ?>
