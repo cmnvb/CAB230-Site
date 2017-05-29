596d1526338bec2ded744df9347b35c34c304328
cmnvb <superstar.kim@hotmail.com>
2017-05-29 18:32:42 +1000

    Fix $pdo error in validatePassword.php
    $pdo was not defined as the global variable.

--- php/validatePassword.php
+++ php/validatePassword.php
@@ -1,16 +1,17 @@
-<?php
-require('connectToDB.php');
+<?php require('connectToDB.php');
 	
 function validatePassword(){
+	global $pdo;
 	$checkPassword = $pdo ->prepare('SELECT Username, Password
 		FROM members
 		WHERE username = :username and
-        password = SHA2(CONCAT(:password, salt), 0)');
-	$checkPassword ->bindValue(':username', $_GET['username']);
-	$checkPassword ->bindValue(':password', $_GET['password']);
-	$checkPassword -> execute();
-	
-	//return $query->rowCount() > 0;
+		password = SHA2(CONCAT(:password, salt), 0)');
 
+	$checkPassword ->bindValue(':username', $_POST['username']);
+	$checkPassword ->bindValue(':password', $_POST['password']);
+
+	$checkPassword -> execute();
+
+	//return $query->rowCount() > 0;
 }
 ?>
